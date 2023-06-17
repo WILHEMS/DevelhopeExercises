9bb075fa14d1967c99729ddd982bbe6430f56cbc
Bolton Derrick <derekwilhems@gmail.com>
2023-06-17 07:11:39 +0300

    updates

---
+++ Exercise1/src/co/develhope/introduction/Start.java
@@ -0,0 +1,7 @@
+package co.develhope.introduction;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.printf("Hello develhope java student June 15");
+    }
+}
