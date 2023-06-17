e6f0aab0384fa5306b08f473f9438e9e870cf0b9
Bolton Derrick <derekwilhems@gmail.com>
2023-06-17 07:20:00 +0300

    java input

---
+++ Exercise1/src/co/develhope/introduction/PrintYourName.java
@@ -0,0 +1,12 @@
+package co.develhope.introduction;
+import java.util.Scanner;
+
+public class PrintYourName {
+    public static void main(String[] args) {
+        Scanner myName = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String yourName = myName.nextLine();
+        System.out.println("Your name is "+ yourName);
+        System.out.printf("Hello develhope java student June 15");
+    }
+}
