4a4b5c4f4f00b1cdc4127ebcfda09621912182b2
Bolton Derrick <derekwilhems@gmail.com>
2023-06-24 18:59:54 +0300

    Using a for loop to print 5 to 25

---
+++ Exercise1/src/co/develhope/introduction/ForLoop.java
@@ -0,0 +1,10 @@
+package co.develhope.introduction;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Print all the numbers from 5 to 25 including the upper and lower bound");
+        for (int i = 5; i <= 25; i++){
+            System.out.println(i);
+        }
+    }
+}
