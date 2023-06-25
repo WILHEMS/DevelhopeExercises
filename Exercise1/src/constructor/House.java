651bb79c729e8b20b58820e63f43951b6a901b6a
Bolton Derrick <derekwilhems@gmail.com>
2023-06-25 16:43:22 +0300

    Exercise updates

---
+++ Exercise1/src/constructor/House.java
@@ -0,0 +1,10 @@
+package constructor;
+
+public class House {
+    String address;
+    Integer numberOfFloors;
+
+    public House(){
+        System.out.println("A house has been created for you from the Finance Bill ");
+    }
+}
---
+++ Exercise1/src/constructor/Tester.java
@@ -0,0 +1,10 @@
+package constructor;
+
+public class Tester {
+    public static void main(String[] args) {
+        House house1;
+        House house2;
+        house1 = new House();
+        house2 = new House();
+    }
+}
