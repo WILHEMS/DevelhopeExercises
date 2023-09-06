8adfda1ced939df2a2180f4a1be8e73c0fea65e5
WILHEMS <derekwilhems@gmail.com>
2023-09-06 11:31:24 +0300

    updates

--- Exercise2/src/javacode/MapSet.java
+++ Exercise2/src/javacode/MapSet.java
@@ -1,4 +1,31 @@
 package javacode;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class MapSet {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> months = new LinkedHashMap<>();
+        months.put("January",1);
+        months.put("February",2);
+        months.put("March",3);
+        months.put("April",4);
+        months.put("May",5);
+        months.put("June",6);
+        months.put("July",7);
+        months.put("August",8);
+        months.put("September",9);
+        months.put("October",10);
+        months.put("November",11);
+        months.put("December",12);
+
+        System.out.println("Set view of keys :"+ months.keySet());
+
+        List<Integer> cardinalMonth = new ArrayList<Integer>(months.values());
+
+        Integer[] cardinalMonthArray = months.values().toArray(new Integer[0]);
+
+        System.out.println("The array is : "+ Arrays.toString(cardinalMonthArray));
+    }
+
 }
